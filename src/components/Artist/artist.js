6b35d4b361ccad147d13df9364216cbcbc133529
AndreyKarpenko <andreykarpenkoua@gmail.com>
2020-04-25 02:03:10 +0300

    add component structure

---
+++ src/components/Artist/artist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Artist = () => {
+  return (
+    <View>
+
+    </View>
+  )
+};
+
+export default Artist;
---
+++ src/screens/Albums/albums.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Album = () => {
+  return (
+    <View>
+      <Text>
+        Album
+      </Text>
+    </View>
+  );
+};
+
+export default Album;
